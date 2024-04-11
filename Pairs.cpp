1cb35b0e497646d6295d25ee11810d3347233807
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-11 12:39:44 +0900

    Update Pairs.cpp

--- Pairs.cpp
+++ Pairs.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
 int pairs(int k, vector<int> arr)
 {
     std::set<int> s;
