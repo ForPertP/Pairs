b567a9d4239ddd29ca7b8c73a017d4fa2ce7c212
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-14 08:44:22 +0900

    Update Pairs.cs

--- Pairs.cs
+++ Pairs.cs
@@ -1,1 +1,30 @@
-cs
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int pairs(int k, List<int> arr)
+    {
+    }
+}
