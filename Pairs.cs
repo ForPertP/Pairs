e14f03d3784782befaf48b90400616fd21b3a1f2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-15 10:01:25 +0900

    Update Pairs.cs

--- Pairs.cs
+++ Pairs.cs
@@ -27,4 +27,28 @@ class Result
     public static int pairs(int k, List<int> arr)
     {
     }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.pairs(k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
